b00009cddcb34b87c6f2ceb70057005c74eca70a
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-10-18 18:38:38 +0530

    save

--- app/LandingPage/[session]/page.js
+++ app/LandingPage/[session]/page.js
@@ -190,39 +190,52 @@ export default function LandingPage({ params }) {
                     value={formData.referral}
                     onChange={handleInputChange}
                     required
-                    placeholder={studentCount && isAdmin ? studentCount:"e.g. https://game.everdawn.io/......"}
+                    placeholder={
+                      studentCount && isAdmin
+                        ? studentCount
+                        : "e.g. https://game.everdawn.io/......"
+                    }
                   />
-                  <h1 style={{color:"white", fontSize: "clamp(0.7rem, 1vw, 1.2rem)", fontFamily:"monospace", margin:"0"}}>Go to: Everdrawn profile &rarr; settings &rarr; referral </h1>
+                  <h1
+                    style={{
+                      color: "white",
+                      fontSize: "clamp(0.7rem, 1vw, 1.2rem)",
+                      fontFamily: "monospace",
+                      margin: "0",
+                    }}
+                  >
+                    Go to: Everdrawn profile &rarr; settings &rarr; referral{" "}
+                  </h1>
                 </section>
               </section>
               <br />
               <button type="submit" className={styles.submitBtn}>
                 Submit
               </button>
+              <button
+                onClick={() => {
+                  signOut({ callbackUrl: "/" }).then(() => {
+                    router.push("/");
+                  });
+                }}
+                className={styles.signOut}
+                type="button"
+              >
+                {screenWidth < 427 ? (
+                  <svg
+                    viewBox="0 0 512 512"
+                    fill="currentColor"
+                    height="1em"
+                    width="1em"
+                  >
+                    <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32v224c0 17.7 14.3 32 32 32s32-14.3 32-32V32zm-144.5 88.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4 0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z" />
+                  </svg>
+                ) : (
+                  "Sign Out"
+                )}
+              </button>
             </form>
           </div>
-
-          <button
-            onClick={() => {
-              signOut({ callbackUrl: "/" }).then(() => {
-                router.push("/");
-              });
-            }}
-            className={styles.signOut}
-          >
-            {screenWidth < 427 ? (
-              <svg
-                viewBox="0 0 512 512"
-                fill="currentColor"
-                height="1em"
-                width="1em"
-              >
-                <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32v224c0 17.7 14.3 32 32 32s32-14.3 32-32V32zm-144.5 88.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4 0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z" />
-              </svg>
-            ) : (
-              "Sign Out"
-            )}
-          </button>
 
           {loading && (
             <h1
