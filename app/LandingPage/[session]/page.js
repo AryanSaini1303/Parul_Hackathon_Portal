85f2d79d004ee1cf352cb4719ea0824de9a21948
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-10-18 18:18:43 +0530

    Feat: added the logout button for mobiles, instruction for referral code and studentCount for admins

--- app/LandingPage/[session]/page.js
+++ app/LandingPage/[session]/page.js
@@ -16,6 +16,38 @@ export default function LandingPage({ params }) {
     purpose: "",
     referral: "",
   });
+  const [studentCount, setStudentCount] = useState();
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    // useEffect doesn't take async function as it's callback directly, instead use the async function inside of it
+    const fetchStudentCount = async () => {
+      try {
+        const response = await fetch("/api/studentCount", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const result = await response.json();
+        setStudentCount(result.count); // Assuming the response is { count: <number> }
+        if (!response.ok) {
+          alert("Error fetching student count");
+        }
+      } catch (error) {
+        console.error("Error getting student count:", error);
+      }
+    };
+    fetchStudentCount(); // Call the async function inside useEffect
+  }, []); // Empty dependency array to run the effect once
+
+  useEffect(() => {
+    if (session) {
+      if (session.user.email == "yograj.rr@gmail.com") {
+        setIsAdmin(true);
+      }
+    }
+  }, [session]);
 
   const [screenWidth, setScreenWidth] = useState();
   useEffect(() => {
@@ -44,10 +76,8 @@ export default function LandingPage({ params }) {
   // Handle form submission
   const handleFormSubmit = async (e) => {
     e.preventDefault();
-
     // Extract email from session
     const email = session?.user?.email; // Ensure session is available
-
     try {
       setLoading(true);
       const response = await fetch("/api/register", {
@@ -60,10 +90,8 @@ export default function LandingPage({ params }) {
           email, // Include email from the session
         }),
       });
-
       const result = await response.json();
       console.log("Response:", result);
-
       if (response.ok) {
         setLoading(false);
         signOut({ callbackUrl: "/" }).then(() => {
@@ -162,8 +190,9 @@ export default function LandingPage({ params }) {
                     value={formData.referral}
                     onChange={handleInputChange}
                     required
-                    placeholder="Your Referral Code"
+                    placeholder={studentCount && isAdmin ? studentCount:"e.g. https://game.everdawn.io/......"}
                   />
+                  <h1 style={{color:"white", fontSize: "clamp(0.7rem, 1vw, 1.2rem)", fontFamily:"monospace", margin:"0"}}>Go to: Everdrawn profile &rarr; settings &rarr; referral </h1>
                 </section>
               </section>
               <br />
@@ -172,29 +201,29 @@ export default function LandingPage({ params }) {
               </button>
             </form>
           </div>
-          {screenWidth > 375 && (
-            <button
-              onClick={() => {
-                signOut({ callbackUrl: "/" }).then(() => {
-                  router.push("/");
-                });
-              }}
-              className={styles.signOut}
-            >
-              {screenWidth < 427 ? (
-                <svg
-                  viewBox="0 0 512 512"
-                  fill="currentColor"
-                  height="1em"
-                  width="1em"
-                >
-                  <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32v224c0 17.7 14.3 32 32 32s32-14.3 32-32V32zm-144.5 88.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4 0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z" />
-                </svg>
-              ) : (
-                "Sign Out"
-              )}
-            </button>
-          )}
+
+          <button
+            onClick={() => {
+              signOut({ callbackUrl: "/" }).then(() => {
+                router.push("/");
+              });
+            }}
+            className={styles.signOut}
+          >
+            {screenWidth < 427 ? (
+              <svg
+                viewBox="0 0 512 512"
+                fill="currentColor"
+                height="1em"
+                width="1em"
+              >
+                <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32v224c0 17.7 14.3 32 32 32s32-14.3 32-32V32zm-144.5 88.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4 0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z" />
+              </svg>
+            ) : (
+              "Sign Out"
+            )}
+          </button>
+
           {loading && (
             <h1
               style={{
