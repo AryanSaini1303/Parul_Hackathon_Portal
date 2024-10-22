26b6c3b2ee257c123d7bd21363b622eb40b3d63b
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-10-22 22:22:06 +0530

    save

--- components/maintenance.js
+++ components/maintenance.js
@@ -10,7 +10,7 @@ const Maintenance = () => {
         <meta name="description" content="Our site is currently under maintenance." />
       </Head>
       <div className={styles.maintenanceContainer}>
-        <h1>We'll Be Back Soon!</h1>
+        <h1>We&apos;ll Be Back Soon!</h1>
         <p>Our website is currently undergoing scheduled maintenance.</p>
         <p>We apologize for any inconvenience caused.</p>
         <div className={styles.spinner}></div>
