3e0e19be7972f8a1b57b893e770c3f587e9cfebc
AryanSaini1303 <saini.aryan9999@gmail.com>
2024-12-07 23:54:49 +0530

    save

--- app/LandingPage/page.js
+++ app/LandingPage/page.js
@@ -12,7 +12,7 @@ export default function LandingPage() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [registrationFlagLoading, setRegistrationFlagLoading] = useState(false);
-  const [registered,setRegistered]=useState(false);
+  const [registered, setRegistered] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const [formData, setFormData] = useState({
     phoneNumber: "",
@@ -75,11 +75,11 @@ export default function LandingPage() {
       };
       fetchRegistrationFlag();
     }
-  }, []);
+  }, [session]);
 
-  useEffect(()=>{
-    registered&&router.push("/TeamsPage")
-  },[registered])
+  useEffect(() => {
+    registered && router.push("/TeamsPage");
+  }, [registered]);
 
   useEffect(() => {
     if (session) {
@@ -194,90 +194,91 @@ export default function LandingPage() {
   if (status == "unauthenticated") return <p>Unauthorised</p>;
 
   return (
-    <>
-      {session ? (
-        !registrationFlagLoading ? (
-          <>
-            <BackgroundVideo />
-            <section
-              className={styles.header}
-              onClick={
-                session && session.user.email == "yograj.rr@gmail.com"
-                  ? downloadStudentData
-                  : null
-              }
-            >
-              <h1>Registration Form</h1>
-            </section>
-            <div className={styles.card}>
-              <form onSubmit={handleFormSubmit}>
-                <div className={styles.firstSection}>
-                  <section>
-                    <label htmlFor="name">Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      value={formData.name}
-                      onChange={handleInputChange}
-                      required
-                      placeholder="e.g. Devam"
-                    />
-                  </section>
-                  <section>
-                    <label htmlFor="phoneNumber">Contact</label>
-                    <input
-                      type="number"
-                      name="phoneNumber"
-                      value={formData.phoneNumber}
-                      onChange={handleInputChange}
-                      required
-                      placeholder="e.g. 9155088251"
-                    />
-                  </section>
-                  <section>
-                    <label htmlFor="university">University</label>
-                    <input
-                      type="text"
-                      name="university"
-                      value={formData.university}
-                      onChange={handleInputChange}
-                      required
-                      placeholder="e.g. ABC University"
-                    />
-                  </section>
-                  <section>
-                    <label htmlFor="github">Github</label>
-                    <input
-                      type="text"
-                      name="github"
-                      value={formData.github}
-                      onChange={handleInputChange}
-                      placeholder="e.g. https://github.com/AryanSaini1303"
-                    />
-                  </section>
-                </div>
-                <br />
-                <section className={styles.secondSection}>
-                  <section className={styles.purpose}>
-                    <label htmlFor={styles.purpose}>
-                      Why do you want to participate?
-                    </label>
-                    <textarea
-                      type="text"
-                      name="purpose"
-                      value={formData.purpose}
-                      onChange={handleInputChange}
-                      required
-                      rows={screenWidth > 426 ? 5 : 3}
-                      cols={screenWidth > 426 ? 5 : 3}
-                      placeholder={
-                        studentCount && isAdmin
-                          ? studentCount
-                          : "e.g. I want to participate because....."
-                      }
-                    />
-                  </section>
-                  {/* <section className={styles.referral}>
+    status == "authenticated" && (
+      <>
+        {session ? (
+          !registrationFlagLoading ? (
+            <>
+              <BackgroundVideo />
+              <section
+                className={styles.header}
+                onClick={
+                  session && session.user.email == "yograj.rr@gmail.com"
+                    ? downloadStudentData
+                    : null
+                }
+              >
+                <h1>Registration Form</h1>
+              </section>
+              <div className={styles.card}>
+                <form onSubmit={handleFormSubmit}>
+                  <div className={styles.firstSection}>
+                    <section>
+                      <label htmlFor="name">Name</label>
+                      <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        required
+                        placeholder="e.g. Devam"
+                      />
+                    </section>
+                    <section>
+                      <label htmlFor="phoneNumber">Contact</label>
+                      <input
+                        type="number"
+                        name="phoneNumber"
+                        value={formData.phoneNumber}
+                        onChange={handleInputChange}
+                        required
+                        placeholder="e.g. 9155088251"
+                      />
+                    </section>
+                    <section>
+                      <label htmlFor="university">University</label>
+                      <input
+                        type="text"
+                        name="university"
+                        value={formData.university}
+                        onChange={handleInputChange}
+                        required
+                        placeholder="e.g. ABC University"
+                      />
+                    </section>
+                    <section>
+                      <label htmlFor="github">Github</label>
+                      <input
+                        type="text"
+                        name="github"
+                        value={formData.github}
+                        onChange={handleInputChange}
+                        placeholder="e.g. https://github.com/AryanSaini1303"
+                      />
+                    </section>
+                  </div>
+                  <br />
+                  <section className={styles.secondSection}>
+                    <section className={styles.purpose}>
+                      <label htmlFor={styles.purpose}>
+                        Why do you want to participate?
+                      </label>
+                      <textarea
+                        type="text"
+                        name="purpose"
+                        value={formData.purpose}
+                        onChange={handleInputChange}
+                        required
+                        rows={screenWidth > 426 ? 5 : 3}
+                        cols={screenWidth > 426 ? 5 : 3}
+                        placeholder={
+                          studentCount && isAdmin
+                            ? studentCount
+                            : "e.g. I want to participate because....."
+                        }
+                      />
+                    </section>
+                    {/* <section className={styles.referral}>
                   <label htmlFor={styles.referral}>
                     Referral code from Everdrawn
                   </label>
@@ -313,37 +314,37 @@ export default function LandingPage() {
                     Above{" "}
                   </h1>
                 </section> */}
-                </section>
-                <br />
-                <button type="submit" className={styles.submitBtn}>
-                  {loading ? "Submitting" : "Submit"}
-                </button>
-                <button
-                  onClick={() => {
-                    signOut({ callbackUrl: "/" }).then(() => {
-                      router.push("/");
-                    });
-                  }}
-                  className={styles.signOut}
-                  type="button"
-                >
-                  {screenWidth < 427 ? (
-                    <svg
-                      viewBox="0 0 512 512"
-                      fill="currentColor"
-                      height="1em"
-                      width="1em"
-                    >
-                      <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32v224c0 17.7 14.3 32 32 32s32-14.3 32-32V32zm-144.5 88.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4 0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z" />
-                    </svg>
-                  ) : (
-                    "Sign Out"
-                  )}
-                </button>
-              </form>
-            </div>
+                  </section>
+                  <br />
+                  <button type="submit" className={styles.submitBtn}>
+                    {loading ? "Submitting" : "Submit"}
+                  </button>
+                  <button
+                    onClick={() => {
+                      signOut({ callbackUrl: "/" }).then(() => {
+                        router.push("/");
+                      });
+                    }}
+                    className={styles.signOut}
+                    type="button"
+                  >
+                    {screenWidth < 427 ? (
+                      <svg
+                        viewBox="0 0 512 512"
+                        fill="currentColor"
+                        height="1em"
+                        width="1em"
+                      >
+                        <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32v224c0 17.7 14.3 32 32 32s32-14.3 32-32V32zm-144.5 88.6c13.6-11.3 15.4-31.5 4.1-45.1s-31.5-15.4-45.1-4.1C49.7 115.4 16 181.8 16 256c0 132.5 107.5 240 240 240s240-107.5 240-240c0-74.2-33.8-140.6-86.6-184.6-13.6-11.3-33.8-9.4-45.1 4.1s-9.4 33.8 4.1 45.1c38.9 32.3 63.5 81 63.5 135.4 0 97.2-78.8 176-176 176s-176-78.8-176-176c0-54.4 24.7-103.1 63.5-135.4z" />
+                      </svg>
+                    ) : (
+                      "Sign Out"
+                    )}
+                  </button>
+                </form>
+              </div>
 
-            {/* {loading && (
+              {/* {loading && (
             <h1
               style={{
                 textAlign: "center",
@@ -356,12 +357,15 @@ export default function LandingPage() {
               Saving User Data
             </h1>
           )} */}
-          </>
-        ):<Loader/>
-      ) : (
-        <Loader/>
-      )}
-      {/* <Maintenance/> */}
-    </>
+            </>
+          ) : (
+            <Loader />
+          )
+        ) : (
+          <Loader />
+        )}
+        {/* <Maintenance/> */}
+      </>
+    )
   );
 }
--- app/TeamsPage/page.js
+++ app/TeamsPage/page.js
@@ -3,12 +3,14 @@ import styles from "./page.module.css";
 import AnimatedGlobe from "@/components/animatedGlobe";
 import BackgroundVideo from "@/components/BackgroundVideo";
 import Button from "@/components/button";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function TeamsPage() {
   const [screenWidth, setScreenWidth] = useState();
+  const { status } = useSession();
+  const router = useRouter();
   useEffect(() => {
     setScreenWidth(window.innerWidth);
     const handleResize = () => {
@@ -19,47 +21,51 @@ export default function TeamsPage() {
       window.removeEventListener("resize", handleResize);
     };
   }, []);
-  const router = useRouter();
+
+  if (status == "unauthenticated") return <p>Unauthorised</p>;
+
   return (
-    <>
-      <BackgroundVideo />
-      <div className={styles.container}>
-        <div className={styles.card}>
-          <section>
-            <div className={styles.info}>
-              <div className={styles.option1}>
-                <h1>Looking to create & lead your own squad?</h1>
-                <Button
-                  text={"Create Team"}
-                  onClick={() => {
-                    router.push("/TeamsPage/Create");
-                  }}
-                />
+    status == "authenticated" && (
+      <>
+        <BackgroundVideo />
+        <div className={styles.container}>
+          <div className={styles.card}>
+            <section>
+              <div className={styles.info}>
+                <div className={styles.option1}>
+                  <h1>Looking to create & lead your own squad?</h1>
+                  <Button
+                    text={"Create Team"}
+                    onClick={() => {
+                      router.push("/TeamsPage/Create");
+                    }}
+                  />
+                </div>
+                <div className={styles.option2}>
+                  <h1>Already got an amazing squad to join?</h1>
+                  <Button
+                    text={"Join Team"}
+                    onClick={() => {
+                      router.push("/TeamsPage/Join");
+                    }}
+                  />
+                </div>
               </div>
-              <div className={styles.option2}>
-                <h1>Already got an amazing squad to join?</h1>
-                <Button
-                  text={"Join Team"}
-                  onClick={() => {
-                    router.push("/TeamsPage/Join");
-                  }}
-                />
-              </div>
-            </div>
-            {screenWidth > 426 && <AnimatedGlobe />}
-          </section>
+              {screenWidth > 426 && <AnimatedGlobe />}
+            </section>
+          </div>
+          <Button
+            text={"Sign Out"}
+            onClick={() => {
+              signOut({ callbackUrl: "/" });
+            }}
+            width={"fit-content"}
+            position={"absolute"}
+            top={"1rem"}
+            right={"1rem"}
+          />
         </div>
-            <Button
-              text={"Sign Out"}
-              onClick={() => {
-                signOut({ callbackUrl: "/" });
-              }}
-              width={"fit-content"}
-              position={"absolute"}
-              top={"1rem"}
-              right={"1rem"}
-            />
-      </div>
-    </>
+      </>
+    )
   );
 }
